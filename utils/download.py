4149e68d65cbf242346d26d9a2ac1965bd7f3c9f
hpysolar <hpysolar@gmail.com>
2014-10-08 09:27:31 +0900

    split download_latest_files

--- utils/download.py
+++ utils/download.py
@@ -219,7 +219,7 @@ def download_http_file(src_url,dst_path=None,post_args=None,overwrite=False,tria
         
         contents = ""
         try:
-            conn = httplib.HTTPConnection(domain_name)
+            conn = httplib.HTTPConnection(domain_name,timeout=75)
             
             if(post_args != None):
                 conn.request("POST", file_path, post_args,headers)
@@ -249,7 +249,7 @@ def download_http_file(src_url,dst_path=None,post_args=None,overwrite=False,tria
     # File saving... 
     if dst_path is not None:
 
-        if is_response == False:
+        if contents == '':
             return False
 
         dst_path2 = make_path(dst_path) + '.down'

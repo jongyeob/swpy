2769b9c45f588ba37cb900f345096ba21fc15628
cme <sos@kasi.re.kr>
2016-04-18 16:52:36 +0900

    SWPY initial

--- __init__.py
+++ __init__.py
@@ -2,35 +2,19 @@
 import os
 from os import path
 import sys
-import logging
+
+_root,_ = path.split(__path__[0])
+SWPY_ROOT = _root
+print "# SWPY_ROOT = {}".format(SWPY_ROOT)
+
+## User configuration import
+try:
+    from swpy_config import *
+    print("# User config imported")
+
+except:
+    print("# User config not imported")
 
 
-## included library
-import sdo
-#import ace ,goes
-import noaa
-#import cactus,seeds
-import dst
-#import wilcox
 
-from swpy import utils
-
-
-LOG = logging.getLogger(__name__)
-LOG_LEVEL = 10
-CONFIG_FILE = 'swpy.ini'
-
-
-def initialize(**kwargs):
-    items = {}
-    try:
-        items = utils.config.load(CONFIG_FILE)
-    except IOError:
-        LOG.debug("Config file is not exist")
-    
-    utils.config.set(globals(),**items.pop(__name__,{}))
-    
-    sdo.initialize(**utils.config.get(items,sdo.__name__))
-    noaa.initialize(**utils.config.get(items,noaa.__name__))
-    dst.initialize(**utils.config.get(items,noaa.__name__))
-    
+   
